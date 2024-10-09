adf81febfd49970c221a68fdba7d92ed2c2f85e3
Mikhail Ushakov <Mikhail.Ushakov@scrdairy.com>
2024-10-09 13:34:00 +0300

    Upload pytorch project

--- HandsOnPyTorch/tensor_operations.py
+++ HandsOnPyTorch/tensor_operations.py
@@ -1,6 +1,9 @@
+import math
+
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
+from torch.autograd import Variable
 
 
 def multiply_tensors(*args):
@@ -39,62 +42,19 @@ def add_tensors(*args):
     return res_t
 
 
-if __name__ == "__main__":
-    #tensor1 = torch.tensor([1.0, 2.0, True])
-    #tensor2 = torch.tensor([-1.0, -5.0, False])
+def test_multiplication():
+    tensor7 = torch.tensor([[[2, 4, 3], [5, 7, 6], [1, 1, 1]],
+                            [[1, 1, 1], [1, 1, 1], [1, 1, 1]]])
+    tensor8 = torch.tensor([[[1, 4, 1], [1, 1, 2], [0, 1, 0]],
+                            [[0, 0, 0], [0, 0, 0], [0, 0, 0]]])
+    mult_tensor = multiply_tensors(tensor7, tensor8)
+    print(f"Multiplication 1 result: {mult_tensor}")
+    print(f"Multiplied shape {mult_tensor.shape}")
+    print(f"Multiplied dimensions {mult_tensor.ndim}")
 
-    #result_add = add_tensors(tensor1, tensor2, "blah blah")
-    #print(result_add)
-    #result_subst = substract_tensors(tensor1, tensor2)
-    #print(result_subst)
+def test_broadcasting():
 
-    #tensor3 = torch.rand(2, 3) * 9 + 1  # random from 0 to 1 normalized to 1 to 10
-    #tensor4 = torch.randint(1, 11, (2, 3))  # integers from 1 to 10
-    #print(f"Tensor 3: {tensor3}")
-    #print(f"Tensor 4: {tensor4}")
-
-    #ceil: the closest integer greater than or equal to each element
-    #print(f"Ceil tensor 3: {torch.ceil(tensor3)}")
-    #print(f"Ceil tensor 4: {torch.ceil(tensor4)}")
-
-    #floor: the closest integer lesser than or equal to each element
-    #print(f"Floor tensor 3: {torch.floor(tensor3)}")
-    #print(f"Floor tensor 4: {torch.floor(tensor4)}")
-
-    #clamp: all tensor values clamped in the provided range in clamp
-    #print(f"Tensor 3 clamp [5:7]: {torch.clamp(tensor3, 5, 7)}")
-    #print(f"Tensor 4 clamp [5:7]: {torch.clamp(tensor4, 5, 7)}")
-
-    #mean: average for tensor
-    #tensor5 = torch.tensor([1, 2, 3, 4, 5, 6], dtype=torch.float32)
-    #print(f"Tensor 5 mean: {tensor5.mean()}")
-    #print(f"Tensor 5 median: {tensor5.median()}")
-
-    #numpi and trigonomical functions
-    #tensor6 = torch.tensor([-np.pi, -np.pi / 2, -1, 0, 1, np.pi / 2, np.pi])
-    #print(f"Tensor 6 sin: {tensor6.sin()}")
-    #print(f"Tensor 6 cos: {tensor6.cos()}")
-
-    #list of evenly spaced numbers in a range
-    #pi_tensor = torch.linspace(-np.pi / 2, np.pi / 2, steps=1000)
-    #pi_spased_sin = pi_tensor.sin()
-    #pi_spased_cos = pi_tensor.cos()
-    #print(pi_spased_sin[0:5])
-    #print(pi_spased_cos[0:5])
-
-    #building a graph
-    #plt.plot(pi_spased_sin, label="Sin")
-    #plt.plot(pi_spased_cos, label="Cos")
-    #plt.show()
-
-    #tensor7 = torch.tensor([[[2, 4, 3], [5, 7, 6], [1, 1, 1]],
-    #                        [[1, 1, 1], [1, 1, 1], [1, 1, 1]]])
-    #tensor8 = torch.tensor([[[1, 4, 1], [1, 1, 2], [0, 1, 0]],
-    #                        [[0, 0, 0], [0, 0, 0], [0, 0, 0]]])
-    #mult_tensor = multiply_tensors(tensor7, tensor8)
-    #print(f"Multiplication 1 result: {mult_tensor}")
-    #print(f"Multiplied shape {mult_tensor.shape}")
-    #rint(f"Multiplied dimensions {mult_tensor.ndim}")
+    #the tensors can be multiplied if all their sizes fit, or if one or more of sub sizes is 1
 
     tensor_ones = torch.ones(4, 3, 2, dtype=torch.int32)
     tensor_rand = torch.floor(torch.rand(3, 2)*10+1).to(torch.int32)
@@ -108,7 +68,79 @@ if __name__ == "__main__":
     print(f"Random tensor: {tensor_rand}")
     print(f"Broadcast tensor: {tensor10}")
 
-    tensor_rand = torch.floor(torch.rand(1, 2)*10+1).to(torch.int32)
+    tensor_rand = torch.floor(torch.rand(4, 3, 1)*10+1).to(torch.int32)
     tensor11 = tensor_ones * tensor_rand
     print(f"Random tensor: {tensor_rand}")
     print(f"Broadcast tensor: {tensor11}")
+
+def test_primitive():
+    tensor1 = torch.tensor([1.0, 2.0, True])
+    tensor2 = torch.tensor([-1.0, -5.0, False])
+
+    result_add = add_tensors(tensor1, tensor2, "blah blah")
+    print(result_add)
+    result_subst = substract_tensors(tensor1, tensor2)
+    print(result_subst)
+
+    tensor3 = torch.rand(2, 3) * 9 + 1  # random from 0 to 1 normalized to 1 to 10
+    tensor4 = torch.randint(1, 11, (2, 3))  # integers from 1 to 10
+    print(f"Tensor 3: {tensor3}")
+    print(f"Tensor 4: {tensor4}")
+
+    #ceil: the closest integer greater than or equal to each element
+    print(f"Ceil tensor 3: {torch.ceil(tensor3)}")
+    print(f"Ceil tensor 4: {torch.ceil(tensor4)}")
+
+    #floor: the closest integer lesser than or equal to each element
+    print(f"Floor tensor 3: {torch.floor(tensor3)}")
+    print(f"Floor tensor 4: {torch.floor(tensor4)}")
+
+    #clamp: all tensor values clamped in the provided range in clamp
+    print(f"Tensor 3 clamp [5:7]: {torch.clamp(tensor3, 5, 7)}")
+    print(f"Tensor 4 clamp [5:7]: {torch.clamp(tensor4, 5, 7)}")
+
+    #mean: average for tensor
+    tensor5 = torch.tensor([1, 2, 3, 4, 5, 6], dtype=torch.float32)
+    print(f"Tensor 5 mean: {tensor5.mean()}")
+    print(f"Tensor 5 median: {tensor5.median()}")
+
+def test_numpy():
+    #numpi and trigonomical functions
+    tensor6 = torch.tensor([-np.pi, -np.pi / 2, -1, 0, 1, np.pi / 2, np.pi])
+    print(f"Tensor 6 sin: {tensor6.sin()}")
+    print(f"Tensor 6 cos: {tensor6.cos()}")
+
+    #list of evenly spaced numbers in a range
+    pi_tensor = torch.linspace(-np.pi / 2, np.pi / 2, steps=1000)
+    pi_spased_sin = pi_tensor.sin()
+    pi_spased_cos = pi_tensor.cos()
+    print(pi_spased_sin[0:5])
+    print(pi_spased_cos[0:5])
+
+    #building a graph
+    plt.plot(pi_spased_sin, label="Sin")
+    plt.plot(pi_spased_cos, label="Cos")
+    plt.show()
+
+
+def test_backward_grad():
+    tensor_base = Variable(torch.ones(2, 2), requires_grad=True)
+    print(f"Base tensor: {tensor_base}")
+    tensor_step2 = tensor_base * 2
+    print(f"Next step tensor: {tensor_step2}")
+    tensor_step3 = torch.pow(tensor_step2, 2)
+    tensor_step4 = tensor_step3.mean()
+    print(f"Yet next tensor {tensor_step3}",tensor_step4)
+    tensor_step4.backward()
+    print(f"Backward base tensor: {tensor_base.grad}")
+
+if __name__ == "__main__":
+    test_primitive()
+
+    #test_numpy()
+
+    #test_multiplication()
+
+    #test_broadcasting()
+
+    #test_backward_grad()
