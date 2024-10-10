27c95bdc21a13607667c72491d82805ad89bd17c
Mikhail Ushakov <Mikhail.Ushakov@scrdairy.com>
2024-10-10 14:27:19 +0300

    Upload pytorch project

---
+++ HandsOnPyTorch/audio.py
@@ -0,0 +1,42 @@
+# import packages and fetch data
+
+import os
+
+import IPython
+import matplotlib
+import matplotlib.pyplot as plt
+import requests
+import torch
+import torchaudio
+import ssl
+
+
+if __name__ == "__main__":
+    matplotlib.rcParams["figure.figsize"] = [16.0, 4.8]
+
+    torch.random.manual_seed(0)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    print(torch.__version__)
+    print(torchaudio.__version__)
+    print(device)
+
+    SPEECH_URL = "https://pytorch-tutorial-assets.s3.amazonaws.com/VOiCES_devkit/source-16k/train/sp0307/Lab41-SRI-VOiCES-src-sp0307-ch127535-sg0042.wav"  # noqa: E501
+    SPEECH_FILE = "_assets/speech.wav"
+
+    if not os.path.exists(SPEECH_FILE):
+        os.makedirs("_assets", exist_ok=True)
+        with open(SPEECH_FILE, "wb") as file:
+            file.write(requests.get(SPEECH_URL).content)
+
+    bundle = torchaudio.pipelines.WAV2VEC2_ASR_BASE_960H
+    print("Sample Rate:", bundle.sample_rate)
+    print("Labels:", bundle.get_labels())
+
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+    # fetch pre-trained weights and load into model
+    model = bundle.get_model().to(device)
+    print(model.__class__)
+
+    IPython.display.Audio(SPEECH_FILE)
